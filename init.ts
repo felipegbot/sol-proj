d9a4b4049e52c370d93bfd55c62afc45cf131f06
Felipe Gabriel Botelho <felipeg.bot@gmail.com>
2024-02-07 09:13:05 -0300

    can read SOL balance

--- init.ts
+++ init.ts
@@ -1,3 +1,17 @@
-import * as solanaWeb3 from "@solana/web3.js";
+import { getKeypairFromEnvironment } from "@solana-developers/helpers";
+import { Connection, LAMPORTS_PER_SOL, PublicKey, clusterApiUrl } from "@solana/web3.js";
+import { configDotenv } from "dotenv";
+configDotenv()
 
-console.log(solanaWeb3)
+const keypair = getKeypairFromEnvironment('SECRET_KEYPAIR')
+
+console.log('✅ keys loaded')
+
+// loads to the devnet cluster
+const connection = new Connection(clusterApiUrl('devnet'))
+
+console.log('Connected and loading current balance of your account')
+connection.getBalance(keypair.publicKey).then((balance) => {
+  const balanceInSol = balance / LAMPORTS_PER_SOL
+  console.log(`💰Your current balance is: ${balanceInSol} SOL`)
+})
